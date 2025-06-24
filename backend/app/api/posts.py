27be4043287efa41a4a30b33204a1c3f08c60121
Zukertort <ricky.gobbi1994@gmail.com>
2025-06-24 14:15:23 -0300

    adding post api

---
+++ backend/app/api/posts.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class PostBase(BaseModel):
+    post_id: int
+    title: str
+    content: str
+    author_id: int
+    published_date: datetime
+
+@app.get("/posts")
+async def all_posts():
+    return {"message": "All posts"}
+
+@app.get("/posts/{post_name}")
+async def get_post(post: PostBase):
+    return {"post_id": post.post_id, "title": post.title, "content": post.content, "author_id": post.author_id, "published_date": post.published_date}
